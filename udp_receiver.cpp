8d41537eb914286524445cde4b5cf5f65081c9fb
Spencer Tung <spencer.zh.tung@gmail.com>
2013-11-28 11:17:45 -0800

    Formatted the code for better readability

--- udp_receiver.cpp
+++ udp_receiver.cpp
@@ -1,248 +1,139 @@
 #include <iostream>
-
 #include <cstring>
-
 #include <sys/socket.h>
-
 #include <sys/types.h>
-
 #include <netdb.h>
-
 #include <fstream>
-
 #include <string>
-
 #include <stdlib.h>
-
 #include <stdio.h>
-
 #include "packet.h"
-
 #define BUFLEN 512
-
 using namespace std;
 
-
-
 // prints the IP address in dotted decimal
-
 void paddr(unsigned char a[])
-
 {
-
 	cout << (int)a[0] << "." << (int)a[1] << "." <<
-
 			(int)a[2] << "." << (int)a[3] << endl;
-
 	// printf("%d.%d.%d.%d\n", a[0], a[1], a[2], a[3]);
-
 }
-
-
 
 void err(char *str)
-
 {
-
     cerr << str << endl;
-
 }
 
-
-
 int main(int argc, char* argv[])
-
 {
-
 	if (argc < 4) {
-
 		cerr << "Usage: " << argv[0] << 
-
 			" <sender_hostname> " << 
-
 			"<sender_portnumber> " <<
-
 			"<filename>" << endl;
-
 		return 0;
-
 	}
 
-
-
 	// the socket addr container
-
 	//struct sockaddr_in myaddr;
-
-    	struct sockaddr_in serv_addr;
-
-    	int i;
+	struct sockaddr_in serv_addr;
+	int i;
 	socklen_t slen=sizeof(serv_addr);
-
-    	char buf[BUFLEN];
-
-
+	char buf[BUFLEN];
 
 	// the host entity container
-
 	struct hostent *hp;
 	struct in_addr **addr_list;
 
 	char* host = argv[1];
 	short portnum = strtol(argv[2], NULL, 10);
-
-
-
 	// strncpy(host, argv[1], sizeof(host)); 
-
-
-
 	cout << "Creating socket..." << endl;
 
 	// Create a socket
-
 	int fd = socket(AF_INET, SOCK_DGRAM, 0);
-
 	if (fd < 0) {
-
 		cerr << "Socket could not be created" << endl;
-
 		return 0;
-
 	}
-
-
 
 	cout << "Binding socket..." << endl;
 
 	/* 
-
 	Bind to arbitrary return addr (client side)
-
 	Client will only send responses
-
 	INADDR_ANY: any IP addr
-
 	0: OS will select the port number
-
 	htonl converts long to network rep (address)
-
 	htons converts short to network rep (port #)
-
 	*/
 
 	memset((char *)&serv_addr, 0, sizeof(serv_addr));
-
 	serv_addr.sin_family = AF_INET;
-
 	serv_addr.sin_port = htons(portnum);
 
-
-
 	cout << "Locating " << host << "..." << endl;
-
 	hp = gethostbyname(host);
-
 	if (!hp) {
-
 		cerr << "Could not resolve " << host << endl;
-
 		return 0;
-
 	}
 
 	for (int i=0; hp -> h_addr_list[i] != 0; i++) {
-
 		cout << "Hostname resolved to: ";
-
 		paddr((unsigned char*) hp -> h_addr_list[i]);
-
 	}
 
-    	
 	//assign the serv_addr from the argv[1]
    	addr_list = (struct in_addr **)hp->h_addr_list;
 
-	
-
-    	if (inet_aton(inet_ntoa(*addr_list[0]), &serv_addr.sin_addr)==0)
-
-    	{
-
-      	  fprintf(stderr, "inet_aton() failed\n");
-
-      	  exit(1);
-
+	if (inet_aton(inet_ntoa(*addr_list[0]), &serv_addr.sin_addr)==0)
+	{
+  	  fprintf(stderr, "inet_aton() failed\n");
+  	  exit(1);
   	}
 
-
-
-   	 //while(1)
-
-   	 //{
-
-      	 	/*
+   	//while(1)
+   	//{
+  	 	/*
 		printf("\nEnter data to send(Type exit and press enter to exit) : ");
-
      	   	scanf("%[^\n]",buf);
-
         	getchar();
-
      	  	if(strcmp(buf,"exit") == 0)
-
           	exit(0);
 
- 
-
         	if (sendto(fd, buf, BUFLEN, 0, (struct sockaddr*)&serv_addr, slen)==-1)
-
             	err("sendto()");
 		*/
-	
-
-    	//}
+	//}
 	
 	/*send file request
-
 	* the first packet is only include file name and header info
-
 	* once receive file data from server, write it into a file
-
 	* send ack and nak
-
 	*/
 	char* filename=argv[3];
  	strcpy(Packet.payload, filename);
 	if (sendto(fd, (void*)&Packet, strlen(filename)+headSize, 0, (struct sockaddr*)&serv_addr, slen)==-1)
-
-            	err("sendto()");
+        err("sendto()");
 	
 	cout<<"creating file"<<endl;
 	string filename1="3.txt";
 	ofstream newfile(filename1.c_str());
-	if (newfile.is_open())
-       {
-		 ssize_t bytes_received;
-		 char buffer[100];
+	if (newfile.is_open()) {
+		ssize_t bytes_received;
+		char buffer[100];
 
-
-
-		 //there has a bug! i should use while instead of if, but when i use while, nothing can output in the newfile. and the bytes_received is 1!!!!!
-		 if ( bytes_received = recvfrom(fd, (void*)buffer,100, 0, (struct sockaddr*)&serv_addr, &slen)!= -1)
-                 {
-                 	 newfile.write(buffer, 13);
-			 cout<<bytes_received<<endl;
-			 cout<<"writing data"<<endl;
-			 cout<<buffer<<endl;
-		 }
-	
-		
-         }                                         
+		//there has a bug! i should use while instead of if, but when i use while, nothing can output in the newfile. and the bytes_received is 1!!!!!
+		if ( bytes_received = recvfrom(fd, (void*)buffer,100, 0, (struct sockaddr*)&serv_addr, &slen)!= -1) {
+     		newfile.write(buffer, 13);
+			cout << bytes_received << endl;
+			cout << "writing data" << endl;
+			cout << buffer << endl;
+		}
+    }                                         
 	newfile.close();	
 
-
 	cout << "Closing socket..." << endl;
-
 	close(fd);
-
 }
--- udp_sender.cpp
+++ udp_sender.cpp
@@ -1,195 +1,124 @@
 #include <iostream>
-
 #include <cstring>
-
 #include <sys/socket.h>
-
 #include <sys/types.h>
-
 #include <netdb.h>
-
 #include <fstream>
-
 #include <string>
-
 #include <stdlib.h>
-
 #include "packet.h"
 #define BUFLEN 512
-
 using namespace std;
 
-
-
 void err(char *str)
-
 {
-
     cerr << str << endl;
-
 }
 
-
-
 int main(int argc, char* argv[])
-
 {
-
 	if (argc < 2) {
-
 		cerr << "Usage: " << argv[0] << 
-
 			" <sender_portnumber> " << endl;
-
 		return 0;
-
 	}
 
-
-
 	// the socket addr container
-
 	struct sockaddr_in server_addr, cli_addr;
-
    	socklen_t slen=sizeof(cli_addr);
-
-    
 
 	cout << "Creating socket..." << endl;
 
 	// Create a socket
-
 	int fd = socket(AF_INET, SOCK_DGRAM, 0);
-
 	if (fd < 0) {
-
 		cerr << "Socket could not be created" << endl;
-
 		return 0;
-
 	}
 
-
-
 	cout << "Binding socket..." << endl;
-
 	/* 
-
 	Bind to arbitrary return addr (client side)
-
 	Client will only send responses
-
 	INADDR_ANY: any IP addr
-
 	htonl converts long to network rep (address/port #)
-
 	*/
 
 	short portnum = strtol(argv[1], NULL, 10);
 
-
-
 	memset((char *)&server_addr, 0, sizeof(server_addr));
-
 	server_addr.sin_family = AF_INET;
-
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
 	server_addr.sin_port = htons(portnum);
 
-
-
 	if (bind(fd, (struct sockaddr *)&server_addr,
-
 			sizeof(server_addr)) < 0) {
-
 		cerr << "Failed to bind to " << portnum << endl;
-
 		return 0;
-
 	} else {
-
 		cout << "Socket bound to port " << portnum << "!"
-
 			 << endl;
-
 	}
-
-
 
     while(1)
-
     {
-
     	cout << "Waiting for data..." << endl;
-
-	ssize_t bytes_received;
+		ssize_t bytes_received;
         if (bytes_received =recvfrom(fd, (void*)&Packet,packetSize, 0, (struct sockaddr*)&cli_addr, &slen)==-1)
-
             err("recvfrom()");
 	
-	cout << "Received packet from: " << inet_ntoa(cli_addr.sin_addr)<<" : ";
-	cout<< ntohs(cli_addr.sin_port)<<endl;
-	cout << "Data: " << Packet.payload<<endl;
+		cout << "Received packet from: " << inet_ntoa(cli_addr.sin_addr)<<" : ";
+		cout<< ntohs(cli_addr.sin_port)<<endl;
+		cout << "Data: " << Packet.payload<<endl;
 
-	//readFile
-	cout<<"reading file"<<endl;
-	//Packet.payload[bytes_received] = '\0';	
-	char buffer[100];
-	int count = 0;
+		//readFile
+		cout<<"reading file"<<endl;
+		//Packet.payload[bytes_received] = '\0';	
+		char buffer[100];
+		int count = 0;
 
-	//get filename;
-	string filename(Packet.payload);
-	cout<<filename<<endl;
-	//read the file from the sender path
-	ifstream fin(filename.c_str(),ios::binary);
+		//get filename;
+		string filename(Packet.payload);
+		cout<<filename<<endl;
+		//read the file from the sender path
+		ifstream fin(filename.c_str(),ios::binary);
 			
-	//file is not exist
-	if(!fin)
-	{
-		char err_msg [] ="the file you request does not exist";
-		//write(fd, err_msg, strlen(err_msg));
-		sendto(fd, err_msg, strlen(err_msg), 0, (struct sockaddr*)&cli_addr, slen);
-		cout<<"File open error!\n";
-		//send(NAK);
-	}
+		//file is not exist
+		if(!fin) {
+			char err_msg [] ="the file you request does not exist";
+			//write(fd, err_msg, strlen(err_msg));
+			sendto(fd, err_msg, strlen(err_msg), 0, (struct sockaddr*)&cli_addr, slen);
+			cout<<"File open error!\n";
+			//send(NAK);
+		}
 
-	/************ start to caculate the fliesize*********/
-	streampos size, beg;
-	int fsize;
-	ifstream fin1(filename.c_str(),ios::binary);
+		/************ start to caculate the fliesize*********/
+		streampos size, beg;
+		int fsize;
+		ifstream fin1(filename.c_str(),ios::binary);
 
-  	// uses a buffer to assess the file size
-	beg = fin1.tellg();
-	fin1.seekg(0, std::ios::end);
-	size = fin1.tellg() - beg;
-	fsize=size;
-	fin1.seekg(beg); // resets the stream pointer to the beginning
-			
-	/************ finish to caculate the fliesize*********/
+	  	// uses a buffer to assess the file size
+		beg = fin1.tellg();
+		fin1.seekg(0, std::ios::end);
+		size = fin1.tellg() - beg;
+		fsize=size;
+		fin1.seekg(beg); // resets the stream pointer to the beginning
+		/************ finish to caculate the fliesize*********/
 	
-	while(!fin.eof())
-	{
-		fin.read(buffer,100);
-		count = fin.gcount();
-		//use write method to send the http response--the content
-		//write(sockfd, buffer, count);
-		sendto(fd, buffer, count, 0, (struct sockaddr*)&cli_addr, slen);
-		cout<<"sending data amount:"<<count<<endl;
-		
-	}
+		while(!fin.eof())
+		{
+			fin.read(buffer,100);
+			count = fin.gcount();
+			//use write method to send the http response--the content
+			//write(sockfd, buffer, count);
+			sendto(fd, buffer, count, 0, (struct sockaddr*)&cli_addr, slen);
+			cout<<"sending data amount:"<<count<<endl;
+		}
 	
-	fin.close();
-	fin1.close();
-
-      
-
+		fin.close();
+		fin1.close();
     }
 
-
-
 	cout << "Closing socket..." << endl;
-
 	close(fd);
-
 }
